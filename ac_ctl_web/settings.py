0d606af2051c1705a12b5067f44a6d37492eb74f
prplecake <me@prplecake.com>
2023-01-12 22:05:03 -0600

    Allow all hosts for now

--- ac_ctl_web/settings.py
+++ ac_ctl_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v4fpz%xwkx02hm0qn!&5#@8kt^hf-gf@7@u_l&qz0t^6w(!a#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
