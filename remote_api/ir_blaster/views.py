31aabe42ad9e789121dc692697905c33e6ba628e
prplecake <me@prplecake.com>
2023-01-13 14:05:21 -0600

    Add ir_blaster api methods

---
+++ remote_api/ir_blaster/views.py
@@ -0,0 +1,28 @@
+from django.http import HttpResponseBadRequest
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from services.ir_blaster import IRBlaster
+
+
+@api_view(['post'])
+def send_once(request):
+    command = request.data['command']
+    if not command:
+        return HttpResponseBadRequest()
+    result = IRBlaster.send_once(command)
+    return Response({'result': result})
+
+
+@api_view(['post'])
+def send_many(request):
+    try:
+        command = request.data['command']
+    except KeyError:
+        return HttpResponseBadRequest()
+    try:
+        duration = request.data['duration']
+    except KeyError:
+        duration = 3
+    result = IRBlaster.send_many(command, duration)
+    return Response({'result': result})
--- remote_api/urls.py
+++ remote_api/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 app_name = 'remote_api'
 urlpatterns = [
-    path('dht/', include(('remote_api.dht.urls', 'dht')))
+    path('dht/', include(('remote_api.dht.urls', 'dht'))),
+    path('ir_blaster/', include(('remote_api.ir_blaster.urls', 'ir_blaster')))
 ]
