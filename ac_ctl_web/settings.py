4b01876665774311e3f2e2acd1af5f290099e0ba
prplecake <me@prplecake.com>
2023-01-13 23:36:33 -0600

    Use env.py for secrets

--- ac_ctl_web/settings.py
+++ ac_ctl_web/settings.py
@@ -10,7 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
+try:
+    from . import env
+except ImportError:
+    raise Exception("An env.py is required to run this application.")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v4fpz%xwkx02hm0qn!&5#@8kt^hf-gf@7@u_l&qz0t^6w(!a#m'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
