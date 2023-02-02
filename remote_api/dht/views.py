3a34f787e1af145d3f6f109b3b5e10f6e7de69cd
prplecake <me@prplecake.com>
2023-02-02 17:44:50 -0600

    Replace erroneously removed `reverse()`

--- remote_api/dht/views.py
+++ remote_api/dht/views.py
@@ -33,7 +33,7 @@ class DhtSensorGraphDataViewSet(viewsets.ModelViewSet):
             limit = 24 * 7
         logger.debug(f'limit: {limit}')
         queryset = DhtSensorData.objects.filter(
-            Q(date__contains=':00:')).order_by('-date')[:limit]
+            Q(date__contains=':00:')).order_by('-date').reverse()[:limit]
         logger.debug(len(queryset))
         return queryset
 
