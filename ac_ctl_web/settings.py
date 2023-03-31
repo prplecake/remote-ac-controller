b559e3e6b19fe477cad6c0bb732ff9fd1690b190
Matthew Jorgensen <me@prplecake.com>
2023-03-31 16:12:51 -0500

    Add log formatting

--- ac_ctl_web/settings.py
+++ ac_ctl_web/settings.py
@@ -168,9 +168,15 @@ if DEBUG:
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s [%(name)s] %(message)s'
+        },
+    },
     'handlers': {
         'console': {
             'class': 'logging.StreamHandler',
+            'formatter': 'default'
         },
     },
     'root': {
