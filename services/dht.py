e6da39ae407b17e040cfd11da854f4c14142052c
Matthew Jorgensen <me@prplecake.com>
2023-03-31 16:13:05 -0500

    Add logging to services/dht.py

--- services/dht.py
+++ services/dht.py
@@ -1,9 +1,11 @@
 # https://peppe8o.com/using-raspberry-pi-with-dht11-temperature-and-humidity-sensor-and-python/
-
+import logging
 from datetime import datetime
 import time
 
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 try:
     # noinspection PyPackageRequirements
@@ -16,10 +18,12 @@ import adafruit_dht
 try:
     dhtDevice = adafruit_dht.DHT11(board.D14)
 except NameError:
+    logger.warning("Could not initialize DHT11 sensor")
     pass
 
 
 def get_dht_data() -> (float, float, str):
+    logger.debug("running 'get_dht_data'")
     temp_c: float = 0.0
     humidity: float = 0.0
     error: str = ''
@@ -32,6 +36,8 @@ def get_dht_data() -> (float, float, str):
         error = err.args[0]
     except NameError as err:
         error = err.args[0]
+
+    logger.warning("'get_dht_data' got error: %s", error)
 
     if error and not settings.DJANGO_ENV == 'development':
         (temp_c, humidity, error) = get_dht_data()
