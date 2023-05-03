b6d6da6f11a31527139bd2b257d93c07e2ac42ba
Matthew Jorgensen <me@prplecake.com>
2023-05-03 18:43:57 -0500

    Implement app state API routes

--- remote_api/urls.py
+++ remote_api/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 app_name = 'remote_api'
 urlpatterns = [
+    path('app/', include(('remote_api.app_state.urls', 'app_state'))),
     path('dht/', include(('remote_api.dht.urls', 'dht'))),
     path('ir_blaster/', include(('remote_api.ir_blaster.urls', 'ir_blaster')))
 ]
