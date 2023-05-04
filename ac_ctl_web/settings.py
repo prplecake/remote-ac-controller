452fa7bf311a67ed26567624b10a8d66625a47c3
Matthew Jorgensen <me@prplecake.com>
2023-05-04 13:25:30 -0500

    Revert removing static files stuff

--- ac_ctl_web/settings.py
+++ ac_ctl_web/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
 
     'corsheaders',
     'django_extensions',
@@ -135,6 +136,12 @@ TIME_ZONE = 'America/Chicago'
 USE_I18N = True
 
 USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = env.STATIC_ROOT
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
