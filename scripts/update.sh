92e98c0b7c7e13a4016e203a4de72cd43fa84fe4
Matthew Jorgensen <me@prplecake.com>
2023-05-04 19:24:02 -0500

    Add update script

---
+++ scripts/update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env -S bash -e
+
+cd /home/pi/remote-ac-controller
+source /home/pi/.zshrc
+source /home/pi/remote-ac-controller/venv/activate
+git pull
+pip install -r requirements.txt
+npm install --omit=dev
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+npm run build
+
+sudo systemctl restart ac-remote-*
