1488837156cc0bf229e9e37439fd47a30cb3a584
Matthew Jorgensen <me@prplecake.com>
2023-05-04 20:08:55 -0500

    collectstatic *after* building webpack...

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,11 +7,9 @@
   git pull
   pip install -r requirements.txt
   npm install --omit=dev
-
+  npm run build
   python manage.py migrate
   python manage.py collectstatic --noinput
-
-  npm run build
 
   sudo systemctl restart ac-remote-*
 )
