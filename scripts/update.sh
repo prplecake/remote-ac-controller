45796ea99560f1c97559936acd3210a8d75c500a
Matthew Jorgensen <me@prplecake.com>
2023-05-04 19:47:30 -0500

    Fix path to `activate`

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,7 +3,7 @@
 (
   cd /home/pi/remote-ac-controller
   source /home/pi/.zshrc
-  source /home/pi/remote-ac-controller/venv/activate
+  source /home/pi/remote-ac-controller/venv/bin/activate
   git pull
   pip install -r requirements.txt
   npm install --omit=dev
