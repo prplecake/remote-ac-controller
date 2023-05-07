330d7f2c24957941c81f91e632bed7cd8fc8d51e
Matthew Jorgensen <me@prplecake.com>
2023-05-07 11:15:35 -0500

    Squash ac_ctl migration and delete remote_web

--- backend/remote_web/__init__.py
+++
@@ -1,5 +0,0 @@
-import warnings
-warnings.warn("the remote_web module is deprecated and will be removed in a "
-              "future update",
-              DeprecationWarning,
-              stacklevel=2)
